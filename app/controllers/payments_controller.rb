be73b354a330201896c0876e050982f1d8f98da9
Smoula <smoula@martin-mbp.smartfin.local>
2019-06-19 13:11:30 +0200

    oprava

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,6 +1,6 @@
 class PaymentsController < ApplicationController
   before_action :set_payment, only: [:show, :edit, :update, :destroy]
-  before_action :set_leasing_contract, only: [:show, :edit, :update, :destroy]
+  before_action :set_leasing_contract, only: [:show, :edit, :update, :destroy, :create]
 
   # GET /payments
   # GET /payments.json
