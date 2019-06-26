2247d0616386c5abea9e25b76bc16640be56dc6b
Smoula <smoula@martin-mbp.smartfin.local>
2019-06-26 12:02:49 +0200

    oprava adresy

--- app/models/address.rb
+++ app/models/address.rb
@@ -3,13 +3,16 @@ class Address < ApplicationRecord
 
 	def completion
 		address = AutocompleterAddress::validate_adres(self.village,self.street,self.number.split("/").first)
-		self.zip = address['psc']
-		self.district = address['okres']
-		self.region = address['kraj']
+		if address 
+			self.zip = address['psc']
+			self.district = address['okres']
+			self.region = address['kraj']
+			self.state = "Čr"
+		end
 	end
 
 	def full_name
-		"#{self.village.to_s} #{self.street.to_s}  #{self.number.to_s} #{self.zip.to_s}  #{self.district.to_s} #{self.region.to_s}" 
+		"#{self.village.to_s} #{self.street.to_s}  #{self.number.to_s} #{self.zip.to_s}  #{self.district.to_s} #{self.region.to_s} #{self.state.to_s}" 
 	end
 	
 	def table_name
