c88b4f6dc26669b59eaa5280cd5a551c20eba31f
Smoula <smoula@martin-mbp.smartfin.local>
2019-06-27 15:24:22 +0200

    opracva

--- app/decorators/invoice_decorator.rb
+++ app/decorators/invoice_decorator.rb
@@ -12,4 +12,9 @@ class InvoiceDecorator < ApplicationDecorator
   def commission_for_the_contract
   	format_number object.rewards.pluck(:commission_for_the_contract).sum
   end
+
+  def agent
+  		object.rewards.first.try(:agent).try(:all_name)
+  end
+
 end
