0b45f4cb0f075cea94378df23ef635c8e261c766
Smoula <smoula@martin-mbp.smartfin.local>
2019-07-24 14:13:12 +0200

    oprava

--- app/validators/person_validator.rb
+++ app/validators/person_validator.rb
@@ -3,7 +3,6 @@ class PersonValidator < ActiveModel::Validator
 	def validate(record)
 		name(record)
 		last_name(record)
-		mobil(record)
 	end
 
 	def name(record)
@@ -12,9 +11,5 @@ class PersonValidator < ActiveModel::Validator
 
 	def last_name(record)
 		record.errors.add(:last_name, "Přijmení je povinný") if record.last_name.blank?
-	end
-
-	def mobil(record)
-		record.errors.add(:mobil, "Mobil je povinný") if record.mobil.blank?
 	end
 end
