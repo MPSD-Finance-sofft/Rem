ad77186830d0f2980eb3db5def346d7f25b872f0
Smoula <smoula@martin-mbp.smartfin.local>
2019-09-23 17:29:00 +0200

    oprava

--- app/assets/javascripts/full_calendar.js
+++ app/assets/javascripts/full_calendar.js
@@ -17,7 +17,7 @@ initialize_calendar = function() {
 
       select: function(start, end) {
         $.getScript('/events/new', function() {
-          $('#event_date_range').val(moment(start).format("MM/DD/YYYY HH:mm") + ' - ' + moment(end).format("MM/DD/YYYY HH:mm"))
+          $('#event_date_range').val(moment(start).format("DD/MM/YYYY HH:mm") + ' - ' + moment(end).format("DD/MM/YYYY HH:mm"))
           date_range_picker();
           $('.start_hidden').val(moment(start).format('YYYY-MM-DD HH:mm'));
           $('.end_hidden').val(moment(end).format('YYYY-MM-DD HH:mm'));
@@ -43,7 +43,7 @@ initialize_calendar = function() {
       
       eventClick: function(event, jsEvent, view) {
         $.getScript(event.edit_url, function() {
-          $('#event_date_range').val(moment(event.start).format("MM/DD/YYYY HH:mm") + ' - ' + moment(event.end).format("MM/DD/YYYY HH:mm"))
+          $('#event_date_range').val(moment(event.start).format("DD/MM/YYYY HH:mm") + ' - ' + moment(event.end).format("DD/MM/YYYY HH:mm"))
           date_range_picker();
           $('.start_hidden').val(moment(event.start).format('YYYY-MM-DD HH:mm'));
           $('.end_hidden').val(moment(event.end).format('YYYY-MM-DD HH:mm'));
