6edb68b0738ee242874f150307965c564f6670c5
Smoula <smoula@martin-mbp.smartfin.local>
2019-10-03 17:30:45 +0200

    oprava

--- config/application.rb
+++ config/application.rb
@@ -10,10 +10,6 @@ module Rem
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-     config.paperclip_defaults = config.paperclip_defaults.merge({
-    	storage: :s3,
-    	path: 'rem/public:url'
-  	})
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
