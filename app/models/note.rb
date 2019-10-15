2430abb98ae0e8f6d536aa7827a0d66e2e00b032
Smoula <smoula@martin-mbp.smartfin.local>
2019-10-15 12:09:10 +0200

    uprava poznamek

--- app/models/note.rb
+++ app/models/note.rb
@@ -2,6 +2,15 @@ class Note < ApplicationRecord
 	include NoteEnum
 	belongs_to :accord
 
+	before_save :ad_default_color
+
+	def ad_default_color
+		if self.color.blank?
+			self.color = "#ff8c82" if permission == "agent"
+			self.color = "#fff86b" if permission == "manager"
+			self.color = "#96d35f" if permission == "user"
+		end
+	end
 
 	scope :for_agent, ->  {where(permission: "agent")}
 	scope :for_manager, ->  {where(permission: ["agent","manager"])}
