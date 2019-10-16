582400fe8e63389cdd6af6394ce80987d7f691ed
Smoula <smoula@martin-mbp.smartfin.local>
2019-10-16 15:13:16 +0200

    oprava

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,5 @@
 class Address < ApplicationRecord
 	def full_name
-		"#{self.street.to_s} #{self.number.to_s} #{self.zip.to_s} #{self.village.to_s}, #{self.district.to_s}, #{self.state.to_s}" 
+		"#{self.street.to_s} #{self.number.to_s} , #{self.zip.to_s} #{self.village.to_s} okr.:#{self.district.to_s}"
 	end
 end
