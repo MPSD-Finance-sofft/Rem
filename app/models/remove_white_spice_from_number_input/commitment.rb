d3619a236c4acb0b913c0dadd823451ffc5b9371
Smoula <smoula@martin-mbp.smartfin.local>
2019-11-12 17:43:55 +0100

    oprava

--- app/models/remove_white_spice_from_number_input/commitment.rb
+++ app/models/remove_white_spice_from_number_input/commitment.rb
@@ -1,11 +1,11 @@
 module RemoveWhiteSpiceFromNumberInput::Commitment
 
-	def date_of_excepted_payment=(value)
-		self[:date_of_excepted_payment] = value.to_s.delete(" ")
+	def real_amount=(value)
+		self[:real_amount] = value.to_s.delete(" ")
 	end
 	
-	def payout_day=(value)
-		self[:payout_day] = value.to_s.delete(" ")
+	def amount=(value)
+		self[:amount] = value.to_s.delete(" ")
 	end
 
 end
