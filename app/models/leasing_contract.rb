3f15fb045eb50c6e99b06d07ed80f32cdd85d159
Smoula <smoula@Martin-MacBook-Pro.local>
2019-11-14 12:46:38 +0100

    oprava

--- app/models/leasing_contract.rb
+++ app/models/leasing_contract.rb
@@ -69,6 +69,7 @@ class LeasingContract < ApplicationRecord
 	
 	scope :for_accord, -> (accord_id) {where(accord_id: accord_id)}
 	scope :contract_number, -> (contract_number) {where(id:  contract_number)}
+	scope :state, -> (state) {where(state:  state)}
 	scope :client_name, -> (client_name) {joins(:clients).where("clients.name": client_name)}
 	scope :client_last_name, -> (client_last_name) {joins(:clients).where("clients.last_name": client_last_name)}
 	scope :realty_type, -> (realty_type_id) {joins(:realty).where("realties.realty_type_id": realty_type_id)}
