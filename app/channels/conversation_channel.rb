2955c0b1ffee1fc53ebcde15728d0b9d73276961
Smoula <smoula@martin-mbp.smartfin.local>
2019-11-19 15:11:19 +0100

    oprava

--- app/channels/conversation_channel.rb
+++
@@ -1,7 +0,0 @@
-class ConversationChannel < ApplicationCable::Channel
-  def subscribed
-    conversation = Conversation.find params[:conversation]
-    binding.pry
-    stream_for conversation
-  end
-end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,5 @@
 class HomeController < ApplicationController
   def index
-  	session[:conversations] ||= []
-
     @users = User.all.where.not(id: current_user)
   end
 
