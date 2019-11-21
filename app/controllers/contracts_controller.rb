329cd2d6a6dc8dae61724a666ac51981ac4eff95
Smoula <smoula@Martin-MacBook-Pro.local>
2019-11-21 10:28:40 +0100

    oprava

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -1,7 +1,7 @@
 class ContractsController < ApplicationController
 
 	def index
-		@accords = policy_scope(Accord).state("contract").order(contract_number: :desc)
+		@accords = policy_scope(Accord).state("contract").order(date_of_signature: :desc)
 	    @accords =  IndexFilter::IndexServices.new(@accords,params).perform
 	    @accords = @accords.decorate
 	    respond_to do |format|
