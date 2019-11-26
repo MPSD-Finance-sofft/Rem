d47e2c86c7db9ab9526ca1fb13d792744774115b
Smoula <smoula@martin-mbp.smartfin.local>
2019-11-26 16:47:32 +0100

    oprava

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,6 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
   config.serve_static_assets = true
-  config.force_ssl = true
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
@@ -49,7 +48,7 @@ Rails.application.configure do
   config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+   config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
