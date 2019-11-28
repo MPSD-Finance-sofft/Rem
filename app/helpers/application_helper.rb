2a3ee15afb2237003acbe1a2ad3df4e8da2780d9
Smoula <smoula@Martin-MacBook-Pro.local>
2019-11-28 12:45:29 +0100

    oprava

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,12 @@ module ApplicationHelper
   def title(page_title)
     content_for(:title) { page_title }
   end
+
+  def check(check)
+  	if check
+		'<i class="fa fa-check" style="color:green"></i>'.html_safe
+	else
+		'<i class="fa fa-times" style="color:red"></i>'.html_safe
+	end
+  end
 end
