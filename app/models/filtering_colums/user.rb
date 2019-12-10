3f40724e1ce0b1a43232671ff613cdc2848656f9
Smoula <smoula@martin-mbp.smartfin.local>
2019-12-10 13:22:10 +0100

    oprava

--- app/models/filtering_colums/user.rb
+++ app/models/filtering_colums/user.rb
@@ -7,23 +7,20 @@ module FilteringColums::User
 			 "created_at",
 			 "updated_at",
 			 "username",
-			 "name",
-			 "last_name",
 			 "name_company",
 			 "identity_card_number",
 			 "identity_company_number",
 			 "date_of_cooperation",
 			 "short_name",
-			 "superior_id",
-			 "title_before",
-			 "title_last",
+			 "superior",
 			 "web",
 			 "account_number",
 			 "bank_code",
 			 "active",
 			 "can_sign_in",
 			 "complate_documentation",
-			 "birthdate"
+			 "birthdate",
+			 "permission_to_s"
 		]
 	end
 end
