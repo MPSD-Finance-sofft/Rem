14d9aab6f9965b645ad3f2fe9d7fa6c1c019fe27
Smoula <smoula@Martin-MacBook-Pro.local>
2019-12-11 10:27:07 +0100

    oprava

--- app/models/user.rb
+++ app/models/user.rb
@@ -114,6 +114,14 @@ class User < ApplicationRecord
 		return if value.blank?
 		super
 	end
+
+	def self.user_can_permision(user)
+		if user.agent? || user.manager?
+			User.can_sign_in.subordinates(user).or(User.where(id: (user.id)))
+		else
+			User.order(username: :desc).can_sign_in.select(&:not_runing_notice?)
+		end
+	end
 		
 	scope :subordinates, -> (user) {where(superior_id: user.id)}
 	scope :username, -> (user) {where("users.username LIKE ? ", "%#{user}%")}
