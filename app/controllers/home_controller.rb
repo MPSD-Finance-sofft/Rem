083d1363814606ca094ba92483d1b63a7657334a
Martin Smolka <martins@fakturoid.cz>
2020-01-13 16:21:53 +0100

    oprava

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @scheduler_logs = SchedulerLog.for_client.decorate
+    @scheduler_logs = SchedulerLog.for_client.order(created_at: :desc).limit(5).decorate
   end
 
   def balance_price_calculation
