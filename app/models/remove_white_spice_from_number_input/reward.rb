1d5308b3131cb6daa2ae5f39c6a58dd5d16c5f21
Martin Smolka <martins@fakturoid.cz>
2020-01-14 15:15:20 +0100

    opravsa

--- app/models/remove_white_spice_from_number_input/reward.rb
+++ app/models/remove_white_spice_from_number_input/reward.rb
@@ -1,11 +1,11 @@
 module RemoveWhiteSpiceFromNumberInput::Reward
 
 	def agency_commission=(value)
-		self[:agency_commission] = value.delete(" ")
+		self[:agency_commission] = value.to_s.delete(" ")
 	end
-	
+
 	def commission_for_the_contract=(value)
-		self[:commission_for_the_contract] = value.delete(" ")
+		self[:commission_for_the_contract] = value.to_s.delete(" ")
 	end
 
 end
