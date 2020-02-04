2fce4bc600652b4bd2f8d6c90182955d34a75b50
Martin Smolka <martins@fakturoid.cz>
2020-02-04 15:56:14 +0100

    opravsa

--- app/models/report.rb
+++ app/models/report.rb
@@ -5,11 +5,11 @@ class Report
 
 	def self.colums_for_user_perrmision(user)
 		if user.admin?
-			User.filtering_attributes_agent + User.filtering_attributes_manager + User.filtering_attributes
+			User.filtering_attributes_manager.merge(User.filtering_attributes_admin)
 		elsif user.manager?
 			User.filtering_attributes_manager
 		else
-			User.filtering_attributes_agent 
+			User.filtering_attributes_agent
 		end
 	end
 end
