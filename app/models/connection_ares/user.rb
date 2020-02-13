d439872f3233d76997541191e3849e0e9d817f62
Martin Smolka <martins@fakturoid.cz>
2020-02-13 15:06:18 +0100

    oprava

--- app/models/connection_ares/user.rb
+++ app/models/connection_ares/user.rb
@@ -2,7 +2,7 @@ module ConnectionAres::User
 
 	def downalod_data_from_ares
 
-		return false if self.identity_company_number.blank?
+		return remove_from_ares if self.identity_company_number.blank?
 
 		uri = URI.escape("http://wwwinfo.mfcr.cz/cgi-bin/ares/darv_bas.cgi?ico=#{self.identity_company_number}")
 		uri = URI.parse(uri)
@@ -11,12 +11,12 @@ module ConnectionAres::User
 
 		help = body['Ares_odpovedi']['Odpoved']['VBAS']
 
-		return false if help.blank?
+		return remove_from_ares if help.blank?
 
 		trades_help = help['Obory_cinnosti']
 
-		return false if trades_help.blank?
-		
+		return remove_from_ares if trades_help.blank?
+
 		trades = trades_help['Obor_cinnosti']
 
 		return false if self.ares.count == trades.size
@@ -27,5 +27,10 @@ module ConnectionAres::User
 			Ares.create(user_id: self.id, nace: trade["K"], nace_name: trade["T"])
 		end
 		true
+	end
+
+	def remove_from_ares
+		Ares.where(user_id: self.id).delete_all
+		false
 	end
 end
