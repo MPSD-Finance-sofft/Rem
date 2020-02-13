4a60189455c71c29f1e03a9131621c23acd5ffd2
Martin Smolka <martins@fakturoid.cz>
2020-02-13 10:59:47 +0100

    uvodni graf

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   def index
     @scheduler_logs = SchedulerLog.for_client.order(created_at: :desc).limit(5).decorate
+    @accords = Accord.all.select("created_at, month(created_at) as month, year(created_at) as year, count(id) as count, user_id, agent_id").group(:month,:year).map{|a| [Date.new(a.year, a.month,1), a.count]} if current_user.admin?
   end
 
   def balance_price_calculation
