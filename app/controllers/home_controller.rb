4fd8bd1ce6c0ee1720563088c07872aa47afee07
Martin Smolka <martins@fakturoid.cz>
2020-02-13 11:17:27 +0100

    oprava

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,19 @@
 class HomeController < ApplicationController
   def index
     @scheduler_logs = SchedulerLog.for_client.order(created_at: :desc).limit(5).decorate
-    @accords = Accord.all.select("created_at, month(created_at) as month, year(created_at) as year, count(id) as count, user_id, agent_id").group(:month,:year).map{|a| [Date.new(a.year, a.month,1), a.count]} if current_user.admin?
+    chart
   end
 
   def balance_price_calculation
   end
+
+
+  private
+  	def chart
+  		if current_user.admin?
+  			accord_data = Accord.all.select("created_at, month(created_at) as month, year(created_at) as year, count(id) as count, user_id, agent_id").group(:month,:year).map{|a| [Date.new(a.year, a.month,1), a.count]}
+    		contract_data = Accord.state('contract').select("created_at, month(date_of_signature) as month, year(date_of_signature) as year, count(id) as count, user_id, agent_id").group(:month,:year).map{|a| [Date.new(a.year, a.month,1), a.count]}
+    		@chart = [{name: 'počet žádostí', data: accord_data }, {name: 'počet smluv', data: contract_data }]
+  		end
+  	end
 end
