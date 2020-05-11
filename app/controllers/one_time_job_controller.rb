99a425001101c444991bf8bd5da1f5dcdd6d1581
Martin Smolka <martins@fakturoid.cz>
2020-05-11 15:08:32 +0200

    oprava

--- app/controllers/one_time_job_controller.rb
+++ app/controllers/one_time_job_controller.rb
@@ -1,6 +1,7 @@
 class OneTimeJobController < ApplicationController
 
   def task_500710
+    Activity.create(true_user_id: user_masquerade_owner.try(:id),user_id: current_user.id, what: "Přehled smlouvy bez smlouvy o prodeji", objet: "task_500710")
     contract =  Accord.contract.pluck(:id)
     contract_with_sales_contract = Accord.with_sales_contract.pluck(:id)
     @contract_without_sales_contract =  Accord.where(id: contract.reject{|x| contract_with_sales_contract.include?(x)})
