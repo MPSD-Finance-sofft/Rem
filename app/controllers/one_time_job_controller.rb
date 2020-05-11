8faf844226b57dd11cde7063c3d58a80feb9173e
Martin Smolka <martins@fakturoid.cz>
2020-05-11 14:47:45 +0200

    oprava

--- app/controllers/one_time_job_controller.rb
+++ app/controllers/one_time_job_controller.rb
@@ -5,6 +5,4 @@ class OneTimeJobController < ApplicationController
     contract_with_sales_contract = Accord.with_sales_contract.pluck(:id)
     @contract_without_sales_contract =  Accord.where(id: contract.reject{|x| contract_with_sales_contract.include?(x)})
   end
-
-
 end
