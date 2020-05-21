cfe429b6e78ab8b1f31460ec6f6e49f7dbce071c
Martin Smolka <martins@fakturoid.cz>
2020-05-21 13:47:19 +0200

    oprava

--- app/models/board.rb
+++ app/models/board.rb
@@ -4,13 +4,14 @@ class Board < ApplicationRecord
 
 	def self.show_by_permission(permission)
 		return 	Board.all if permission == "admin" || permission == "user"
-		return 	Board.for_manager if permission == "manager" 
-		return 	Board.for_permmision("agent") if permission == "agent" 
-    return  Board.for_permmision("tipster") if permission == "tipster" 
+		return 	Board.for_manager if permission == "manager"
+		return 	Board.for_agent if permission == "agent"
+    return  Board.for_permmision("tipster") if permission == "tipster"
 	end
 
 	scope :for_permmision, -> (permision) {where(permission: permision)}
 	scope :for_manager, ->  {where(permission: ["manager", "agent", "tipster"])}
+  scope :for_agent, ->  {where(permission: ["agent", "tipster"])}
 	scope :validity, -> {where("date <= ? AND end_date >= ?", Date.today,Date.today)}
 
 end
