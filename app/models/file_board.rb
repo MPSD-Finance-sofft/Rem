5e2582b4a9d85a9084ea53a0b9b36aceba303119
Martin Smolka <martins@fakturoid.cz>
2020-05-21 15:19:11 +0200

    oprava

--- app/models/file_board.rb
+++ app/models/file_board.rb
@@ -4,15 +4,15 @@ class FileBoard < ApplicationRecord
 	def self.show_by_permission(permission)
 		return 	FileBoard.all if permission == "admin" || permission == "user"
 		return 	FileBoard.for_manager if permission == "manager"
-		return 	FileBoard.for_permmision("agent") if permission == "agent"
+		return 	FileBoard.for_agent if permission == "agent"
     return  FileBoard.for_permmision("tipster") if permission == "tipster"
 	end
 
 	enum permission: [:agent, :manager, :user, :admin, :tipster]
 
 
-	scope :for_agent, ->  {where(permission: "agent")}
-	scope :for_manager, ->  {where(permission: ["agent","manager"])}
+	scope :for_manager, ->  {where(permission: ["agent","manager", "tipster"])}
+  scope :for_agent, ->  {where(permission: ["agent","tipster"])}
 	scope :validity, -> {where("start <= ? AND end >= ?", Date.today,Date.today)}
 	scope :for_permmision, -> (permision) {where(permission: permision)}
 end
