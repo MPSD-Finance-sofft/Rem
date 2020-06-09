b67e56fa5db25bcf796fde017fe28d692f66e115
Martin Smolka <martins@fakturoid.cz>
2020-06-09 13:13:05 +0200

    oprava

--- app/controllers/full_notes_controller.rb
+++ app/controllers/full_notes_controller.rb
@@ -1,6 +1,6 @@
 class FullNotesController < ApplicationController
   def index
-    return redirect_to root_path, alert: "Nemáte potřebná oprávnění" if  !current_user.admin? || !current_user.user?
+    return redirect_to root_path, alert: "Nemáte potřebná oprávnění" if  !(current_user.admin? || current_user.user?)
     accord = Accord.find params[:accord_id]
     @accord_notes = Note.for_accord(params[:accord_id]).decorate
     @note_leasing_contract = NoteLeasingContract.for_accord(accord.leasing_contracts.pluck(:id)).decorate
