fd55c88749be4e1ba6ad99257858ee6af901dfe2
Martin Smolka <martins@fakturoid.cz>
2020-06-19 11:49:43 +0200

    oprava

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -5,7 +5,7 @@ class ExpensesController < ApplicationController
   # GET /expenses.json
   def index
     return redirect_to root_path, alert: "Nemáte potřebná oprávnění" if  !(current_user.admin? || current_user.user?)
-    @expenses = Expense.includes(accord: :address).includes(:expense_type).decorate
+    @expenses = Expense.includes(accord: :address).includes(:expense_type).order(accord_id: :desc).decorate
     Activity.create(true_user_id: user_masquerade_owner.try(:id),user_id: current_user.id, what: "Přehled nákladů", objet: "expense")
   end
 
