8bebea08793d6f2c4c37e5ed5ceb8547efb765b2
Martin Smolka <martins@fakturoid.cz>
2021-01-22 12:34:42 +0100

    Fix.

--- app/models/reports/leasing_contracts.rb
+++ app/models/reports/leasing_contracts.rb
@@ -4,27 +4,31 @@ module Reports::LeasingContracts
   end
 
   def sum_purchace_price_for_period(from, to)
-    Accord.where(state: 'contract').where("date_of_signature <= ? AND date_of_signature >= ?", to,from).sum(:purchase_price)
+    Accord.where(state: 'contract').where.not(id: sales_contract_from_period(from, to)).where("date_of_signature <= ? AND date_of_signature >= ?", to,from).sum(:purchase_price)
   end
 
   def count_purchace_price_for_period(from, to)
-    Accord.where(state: 'contract').where("date_of_signature <= ? AND date_of_signature >= ?", to,from).count
+    Accord.where(state: 'contract').where.not(id: sales_contract_from_period(from, to)).where("date_of_signature <= ? AND date_of_signature >= ?", to,from).count
   end
 
   def count_leasing_contract_from_period(from, to )
-    LeasingContract.where("rent_from <= ? AND rent_from >= ?",to, from).count
+    LeasingContract.where.not(accord_id: sales_contract_from_period(from, to)).where("rent_from <= ? AND rent_from >= ?",to, from).count
   end
 
   def sum_purchace_price_leasing_contract_from_period(from, to)
-    LeasingContract.where("rent_from <= ? AND rent_from >= ?",to, from).joins(:accord).sum(:purchase_price)
+    LeasingContract.where.not(accord_id: sales_contract_from_period(from, to)).where("rent_from <= ? AND rent_from >= ?",to, from).joins(:accord).sum(:purchase_price)
   end
 
   def sum_monthly_rent_leasing_contract_from_period(from, to)
-    LeasingContract.where("rent_from <= ? AND rent_from >= ?",to, from).sum(:monthly_rent)
+    LeasingContract.where.not(accord_id: sales_contract_from_period(from, to)).where("rent_from <= ? AND rent_from >= ?",to, from).sum(:monthly_rent)
   end 
 
   def sum_payments_rent_leasing_contract_from_period(from, to)
     Payment.where("payment_date <= ? AND payment_date >= ?", to, from).sum(:amount)
+  end
+
+  def sales_contract_from_period(from, to)
+    SalesContract.where("date_of_sale_realty <= ? AND date_of_sale_realty >= ?", to, from).pluck(:accord_id)
   end
 
 end
