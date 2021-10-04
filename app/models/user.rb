9ee0f0a0642b9efffd1152a29fb06f4371f137ff
Martin Smolka <martins@fakturoid.cz>
2021-10-04 14:21:15 +0200

    fix:

--- app/models/user.rb
+++ app/models/user.rb
@@ -212,7 +212,7 @@ class User < ApplicationRecord
   scope :agents_and_tipster,   ->{ joins(:permission).where('permissions.kind': ['agent', 'tipster']) }
 	scope :manager,   ->{ joins(:permission).where('permissions.kind': 'manager') }
 	scope :can_sign_in, -> {where(can_sign_in: true)}
-  scope :can_sign_in, -> (value) {where(can_sign_in: value)}
+  scope :can_sign_in_with_params, -> (value) {where(can_sign_in: value)}
 	scope :all_without_user,   ->{ joins(:permission).where('permissions.kind': ['manager','user','agent', 'tipster']) }
   scope :user_ares,   -> (nace) { joins(:ares).where('ares.nace_name': nace).distinct }
 
