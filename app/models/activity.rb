a6344201112d7f2ed427c616c00e57a599c1bfd1
Martin Smolka <martins@fakturoid.cz>
2022-06-15 18:31:45 +0200

    Fix birthsday.

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,32 +1,34 @@
+# frozen_string_literal: true
+
 class Activity < ApplicationRecord
-	has_paper_trail ignore: [:updated_at]
-	belongs_to :user
-	belongs_to :true_user, foreign_key: 'true_user_id', class_name: 'User',  required: false
+  has_paper_trail ignore: [:updated_at]
+  belongs_to :user
+  belongs_to :true_user, class_name: 'User', optional: true
 
-	def self.duplicate
-		Activity.all.group_by(&:filter).select{|k,v| v.size > 1}
-	end
+  def self.duplicate
+    Activity.all.group_by(&:filter).select { |_k, v| v.size > 1 }
+  end
 
-	def self.delete_duplicate
-		list = []
-		Activity::duplicate.each do |duplicate|
-			data = duplicate.last
-			activity_ids = data.map(&:id)
-			activity_ids.pop
-			list << activity_ids
-			Activity.where(id: activity_ids).delete_all
-		end
-		SchedulerLog.create(kind: 'Activity', list: list) unless list.blank?
-	end
+  def self.delete_duplicate
+    list = []
+    Activity.duplicate.each do |duplicate|
+      data = duplicate.last
+      activity_ids = data.map(&:id)
+      activity_ids.pop
+      list << activity_ids
+      Activity.where(id: activity_ids).delete_all
+    end
+    SchedulerLog.create(kind: 'Activity', list: list) if list.present?
+  end
 
-	def filter
-		self.created_at.day.to_s + self.created_at.hour.to_s + self.created_at.year.to_s + self.user_id.to_s + self.object_id.to_s + self.objet.to_s
-	end
+  def filter
+    self.created_at.day.to_s + self.created_at.hour.to_s + self.created_at.year.to_s + self.user_id.to_s + self.object_id.to_s + self.objet.to_s
+  end
 
-	scope :for_user, -> (user_id) {where(user_id:  user_id)}
-	scope :start_created_at, -> (date) {where("activities.created_at > ?", date.to_date)}
-	scope :end_created_at, -> (date) {where("activities.created_at < ?", date.to_date)}
-	scope :user_id, -> (user_id) {where(user_id:  user_id)}
-	scope :objet, -> (objet) {where(objet:  objet)}
-	scope :object_id, -> (object_id) {where(object_id:  object_id)}
+  scope :for_user, ->(user_id) { where(user_id: user_id) }
+  scope :start_created_at, ->(date) { where("activities.created_at > ?", date.to_date) }
+  scope :end_created_at, ->(date) { where("activities.created_at < ?", date.to_date) }
+  scope :user_id, ->(user_id) { where(user_id: user_id) }
+  scope :objet, ->(objet) { where(objet:  objet) }
+  scope :object_id, ->(object_id) { where(object_id: object_id) }
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,35 +1,38 @@
+# frozen_string_literal: true
+
 class Event < ApplicationRecord
-	validates :title, presence: true
-  	attr_accessor :date_range
+  attr_accessor :date_range
 
-  	belongs_to :creator, class_name: "User", foreign_key: 'creator_id'
-    belongs_to :user, class_name: "User", foreign_key: 'user_id'
+  validates :title, presence: true
 
-  	def all_day_event?
-    	self.start == self.start.midnight && self.end == self.end.midnight ? true : false
-  	end
+  belongs_to :creator, class_name: "User"
+  belongs_to :user, class_name: "User"
 
-  	def event_text_with_accord(accord_id)
-  		#self.title = self.title + "<a href='/accords/#{accord_id}'>Žádost</a>"
-  	end
+  def all_day_event?
+    self.start == self.start.midnight && self.end == self.end.midnight ? true : false
+  end
 
-    def self.create_birthday
-      User.can_sign_in.manager_and_agents_and_tipster.where("birthdate IS NOT NULL").each do |user|
-        date = Date.new(Date.today.year, user.birthdate.month, user.birthdate.day)
+  def event_text_with_accord(accord_id)
+    # self.title = self.title + "<a href='/accords/#{accord_id}'>Žádost</a>"
+  end
 
-        if Event.where(title: "Narozeniny").where(creator_id: user.id).where(start: date.beginning_of_day..date.end_of_day).blank?
-          Event.create(user_id: 1, title: "Narozeniny", creator_id: user.id, start: date, end: date, text: "Narozeniny #{user.all_name}", color: "#f5f542")
-          Event.create(user_id: 18720, title: "Narozeniny", creator_id: user.id, start: date, end: date, text: "Narozeniny #{user.all_name}", color: "#f5f542")
-          Event.create(user_id: 31523, title: "Narozeniny", creator_id: user.id, start: date, end: date, text: "Narozeniny #{user.all_name}", color: "#f5f542")
-        end
+  def self.create_birthday
+    User.can_sign_in.manager_and_agents_and_tipster.where.not(birthdate: nil).find_each do |user|
+      date = Date.new(Date.today.year, user.birthdate.month, user.birthdate.day)
+
+      if Event.where(title: "Narozeniny").where(creator_id: user.id).where(start: date.beginning_of_day..date.end_of_day).blank?
+        Event.create(user_id: 1, title: "Narozeniny", creator_id: user.id, start: date, end: date, text: "Narozeniny #{user.all_name}", color: "#f5f542")
+        Event.create(user_id: 18720, title: "Narozeniny", creator_id: user.id, start: date, end: date, text: "Narozeniny #{user.all_name}", color: "#f5f542")
+        Event.create(user_id: 31523, title: "Narozeniny", creator_id: user.id, start: date, end: date, text: "Narozeniny #{user.all_name}", color: "#f5f542")
       end
     end
+  end
 
-	scope :for_user, -> (user_id) {where(user_id:  user_id)} 
-  scope :subordinates_events, -> (user) {where(user_id: [User.where(superior_id: user.id).pluck(:id)])}
-  scope :done, -> (done) {where(done:  done)} 
-  scope :title, -> (title) {where(title:  title)} 
-  scope :creator, -> (creator_id) {where(creator:  creator_id)} 
-  scope :end_date, -> (end_date) {where('end <= ?', end_date.to_date + 1.day)} 
-  scope :start_date, -> (start_date) {where('start >= ?', start_date.to_date+ 1.day)} 
+  scope :for_user, ->(user_id) { where(user_id: user_id) }
+  scope :subordinates_events, ->(user) { where(user_id: [User.where(superior_id: user.id).pluck(:id)]) }
+  scope :done, ->(done) { where(done: done) }
+  scope :title, ->(title) { where(title: title) }
+  scope :creator, ->(creator_id) { where(creator: creator_id) }
+  scope :end_date, ->(end_date) { where('end <= ?', end_date.to_date + 1.day) }
+  scope :start_date, ->(start_date) { where('start >= ?', start_date.to_date+ 1.day) }
 end
