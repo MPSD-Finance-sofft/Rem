29a974e470d385ab9b357f25857202dd97f8b942
Martin Smolka <martinsmolka155@gmail.com>
2025-10-06 20:39:02 +0200

    Fix migrate.

--- db/migrate/20190924083858_create_file_boards.rb
+++ db/migrate/20190924083858_create_file_boards.rb
@@ -5,7 +5,11 @@ class CreateFileBoards < ActiveRecord::Migration[5.2]
       t.integer :permission
       t.date :end
       t.date :start
-      t.attachment :file
+      # Místo t.attachment :file použijte:
+      t.string :file_file_name
+      t.string :file_content_type
+      t.integer :file_file_size
+      t.datetime :file_updated_at
       t.timestamps
     end
   end
