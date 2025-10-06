2732ecba6004e877adcfe582b453b8f6dc401999
Martin Smolka <martinsmolka155@gmail.com>
2025-10-06 18:46:28 +0200

    Fix deploy.

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,6 +4,10 @@
 set :application, "rem"
 set :repo_url, "https://github.com/MPSD-Finance-sofft/Rem.git"
 
+# Definice stage
+set :stages, %w(production production_circolus)
+set :default_stage, "production"
+
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
 
@@ -13,18 +17,12 @@ set :linked_files, %w(config/database.yml)
 
 set :pty,             true
 set :use_sudo,        false
-set :stage,           :production
 set :deploy_via,      :remote_cache
 set :deploy_to, '/opt/apps/rem'
-set :puma_bind,       "tcp://0.0.0.0:3000"  # Production na portu 3000
-set :puma_state,      "#{shared_path}/tmp/pids/puma_production.state"
-set :puma_pid,        "#{shared_path}/tmp/pids/puma_production.pid"
-set :puma_access_log, "#{release_path}/log/puma_production.error.log"
-set :puma_error_log,  "#{release_path}/log/puma_production.access.log"
 set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
-set :puma_init_active_record, false  # Change to true if using ActiveRecord
+set :puma_init_active_record, false
 set :whenever_roles,  :app
 set :whenever_environment, ->{ fetch(:rails_env) }
 set :whenever_identifier,  ->{ "#{fetch(:application)}_#{fetch(:stage)}" }
--- config/deploy/production_circolus.rb
+++ config/deploy/production_circolus.rb
@@ -1,69 +1,7 @@
 # config/deploy/production_circolus.rb
-# config valid for current version and patch releases of Capistrano
-
-set :application, "rem"
-set :repo_url, "https://github.com/MPSD-Finance-sofft/Rem.git"
-
-set :puma_threads,    [4, 16]
-set :puma_workers,    0
-
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "storage"
-append :rbenv_map_bins, 'puma', 'pumactl'
-set :linked_files, %w(config/database.yml)
-
-set :pty,             true
-set :use_sudo,        false
-set :stage,           :production_circolus
-set :deploy_via,      :remote_cache
-set :deploy_to, '/opt/apps/rem'
-set :puma_bind,       "tcp://0.0.0.0:4000"  # Circolus na portu 4000
-set :puma_state,      "#{shared_path}/tmp/pids/puma_circolus.state"
-set :puma_pid,        "#{shared_path}/tmp/pids/puma_circolus.pid"
+set :stage, :production_circolus
+set :puma_bind, "tcp://0.0.0.0:4000"
+set :puma_state, "#{shared_path}/tmp/pids/puma_circolus.state"
+set :puma_pid, "#{shared_path}/tmp/pids/puma_circolus.pid"
 set :puma_access_log, "#{release_path}/log/puma_circolus.error.log"
-set :puma_error_log,  "#{release_path}/log/puma_circolus.access.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
-set :puma_preload_app, true
-set :puma_worker_timeout, nil
-set :puma_init_active_record, false  # Change to true if using ActiveRecord
-set :whenever_roles,  :app
-set :whenever_environment, ->{ fetch(:rails_env) }
-set :whenever_identifier,  ->{ "#{fetch(:application)}_#{fetch(:stage)}" }
-
-namespace :puma do
-  desc 'Create Directories for Puma Pids and Socket'
-  task :make_dirs do
-    on roles(:app) do
-      execute "mkdir #{shared_path}/tmp/sockets -p"
-      execute "mkdir #{shared_path}/tmp/pids -p"
-    end
-  end
-end
-
-namespace :deploy do
-  desc "Make sure local git is in sync with remote."
-  task :check_revision do
-    on roles(:app) do
-      unless `git rev-parse HEAD` == `git rev-parse origin/master`
-        puts "WARNING: HEAD is not the same as origin/master"
-        puts "Run `git push` to sync changes."
-        exit
-      end
-    end
-  end
-
-  desc "Clear crontab"
-  task :clear_crontab do
-    on roles(:app) do
-      within release_path do
-        execute :bundle, :exec, :whenever, "--clear-crontab", fetch(:application)
-      end
-    end
-  end
-
-  before :starting, :check_revision
-  after  :deploy,  'deploy:clear_crontab'
-end
-
-# ps aux | grep puma    # Get puma pid
-# kill -s SIGUSR2 pid   # Restart puma
-# kill -s SIGTERM pid   # Stop puma
+set :puma_error_log, "#{release_path}/log/puma_circolus.access.log"
